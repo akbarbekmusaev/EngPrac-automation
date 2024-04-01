a9390e75a86840139ee04eb3ae7ff0bf81521015
akbarbekmusaev <162443882+akbarbekmusaev@users.noreply.github.com>
2024-04-01 23:53:06 +0100

    Initial model of the automation code

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,32 +1,18 @@
 
-const int sensorMin = 0;    // sensor minimum, discovered through experiment
-const int sensorMax = 600;  // sensor maximum, discovered through experiment
-
 void setup() {
-  // initialize serial communication:
-  Serial.begin(9600);
+  pinMode(closingButtonPin, INPUT_PULLUP);
+  pinMode(openingButtonPin, INPUT_PULLUP);
+  pinMode(emergencyButtonPin, INPUT_PULLUP);
+  
 }
 
-void loop() {
-  // read the sensor:
-  int sensorReading = analogRead(A0);
-  // map the sensor range to a range of four options:
-  int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
+const int closingButtonPin =  5;
+const int openingButtonPin =  6; 
+const int emergencyButtonPin =  7; 
 
-  // do something different depending on the range value:
-  switch (range) {
-    case 0:  // your hand is on the sensor
-      Serial.println("dark");
-      break;
-    case 1:  // your hand is close to the sensor
-      Serial.println("dim");
-      break;
-    case 2:  // your hand is a few inches from the sensor
-      Serial.println("medium");
-      break;
-    case 3:  // your hand is nowhere near the sensor
-      Serial.println("bright");
-      break;
-  }
-  delay(1);  // delay in between reads for stability
+void loop() {
+  int closingButtonState = digitalRead(closingButtonPin);
+  int openingButtonState = digitalRead(openingButtonPin);
+  int emergencyButtonState = digitalRead(emergencyButtonPin);
+
 }
